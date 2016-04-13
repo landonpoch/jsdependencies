a34ac71790a6980ecb657fa22f1d0886cf6a207e
Landon Poch <landonpoch@Landons-MacBook-Pro.local>
2016-04-13 11:41:05 -0600

    Renaming some variables for clarity.

--- client/ioc.js
+++ client/ioc.js
@@ -12,27 +12,35 @@
 // the UI codebases can add a reference to CommonJS fairly easily.
 var requirejs = require('requirejs');
 
+// Create our own container.  We have complete flexibility over which container we want to use
+// currently using requirejs because I don't know of a better one to use yet.
+var container = {
+    register: requirejs.define,
+    resolve: requirejs
+}
+
 // loads the 2 packages we need
 var csl = require('services');
 var apjs = require('player');
 
-// This is an example of how we could register certain modules inside of our packages.
-// This would be similar to the IoC modules we had in C#
-apjs.IoC(requirejs);
-
-// We could create an IoC module for CSL also
-// csl.IoC(requirejs);
-
 // Modules that the UI needs to decide should be registered in the UI project instead
 // of in IoC modules within packages.
 // Defines the requestHelper named dependency
-requirejs.define('requestHelper', function() {
+// Takes priority over defaults wich may be registered by calling IoC on a given module
+container.register('requestHelper', function() {
     // Calls the RequestHelper's constructor passing in no dependencies
     // You can choose which requestHelper you want to use in the executing assembly.
     
     return new csl.RequestHelper();
     //return new apjs.RequestHelper();
 });
+
+// This is an example of how we could register certain modules inside of our packages.
+// This would be similar to the IoC modules we had in C#
+apjs.IoC(container);
+
+// We could create an IoC module for CSL also
+// csl.IoC(container);
 
 // Create a convenience method for getting a module / modules
 var moduleGetter = function(modulesArray, callback) {
